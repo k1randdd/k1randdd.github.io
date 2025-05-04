404deb9e4b1a2c90f7972c221ca5264be6d35d8a
Rafaelioso <116551006+Rafaelioso@users.noreply.github.com>
2025-05-04 19:41:30 +0100

    Update marine_animal_distress_image_gallery_table.js

--- scripts/marine_animal_distress_image_gallery_table.js
+++ scripts/marine_animal_distress_image_gallery_table.js
@@ -2,10 +2,10 @@
 // Array of: [<image-file-path>, <image-width-in-pixels>, <image-height-in-pixels>, <image-description-text>]
 
 marine_animal_distress_image_gallery = [
-  ["../images/turtle_straw_bag.jpg", "Turtle with straw stuck in its mouth"],
-  ["../images/seal_plastic_tied.jpg", "Seal entangled in plastic"],
-  ["../images/seahorse_plastic.jpg", "Seahorse swimming around plastic"],
-  ["../images/fish_straw.jpg", "Fish with straw stuck in mouth"],
-  ["../images/crab_and_plastic_net.jpg", "Crab and plastic net"],
-  ["../images/turtle_stuck_in_plastic_wiring.jpg", "Turtle entangled in plastic wiring"],
+  ["../images/turtle_straw_bag.jpg", "A turtle with plastic obcstructing its breathing capability"],
+  ["../images/seal_plastic_tied.jpg", "Seal entangled in plastic which tears at the skin as they grow and move around"],
+  ["../images/seahorse_plastic.jpg", "Seahorse clinging to a cotton swab indicating how easily marine life mistake plastic for natural debris"],
+  ["../images/fish_straw.jpg", "Fish with straw stuck in mouth obstructing breathing and feeding capability"],
+  ["../images/crab_and_plastic_net.jpg", "Turtle trapped in a plastic net tearing at the skin and restricting turtles movement"],
+  ["../images/turtle_stuck_in_plastic_wiring.jpg", "Turtle entangled in plastic wiring restricting movement and breathing capability"],
 ];
