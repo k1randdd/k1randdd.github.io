6e83efba66422b998c68162eecab1dab7b2490a1
Rafaelioso <116551006+Rafaelioso@users.noreply.github.com>
2025-05-05 12:13:02 +0100

    Update video_sources_table.js

--- scripts/video_sources_table.js
+++ scripts/video_sources_table.js
@@ -1,5 +1,4 @@
-// *** Video Sources table. ***
-// Array of: [<hyperlink-text>, <hyperlink-url>]
+
 
 video_sources = [
   ["National Geographic: How We Can Keep Plastics Out of Our Ocean", "https://www.youtube.com/watch?v=HQTUWK7CM-Y&t=3s"],
