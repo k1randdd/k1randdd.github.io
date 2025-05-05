257d3409d00ee2315d6854be72a6a6613bf69f30
Rafaelioso <116551006+Rafaelioso@users.noreply.github.com>
2025-05-05 12:10:20 +0100

    Update marine_animal_distress_image_gallery_table.js

--- scripts/marine_animal_distress_image_gallery_table.js
+++ scripts/marine_animal_distress_image_gallery_table.js
@@ -2,7 +2,7 @@
 // Array of: [<image-file-path>, <image-width-in-pixels>, <image-height-in-pixels>, <image-description-text>]
 
 marine_animal_distress_image_gallery = [
-  ["../images/turtle_straw_bag.jpg", "A turtle with plastic obcstructing its breathing capability"],
+  ["../images/turtle_straw_bag.jpg", "Turtle with plastic obcstructing its breathing capability"],
   ["../images/seal_plastic_tied.jpg", "Seal entangled in plastic which tears at the skin as they grow and move around"],
   ["../images/seahorse_plastic.jpg", "Seahorse clinging to a cotton swab indicating how easily marine life mistake plastic for natural debris"],
   ["../images/fish_straw.jpg", "Fish with straw stuck in mouth obstructing breathing and feeding capability"],
